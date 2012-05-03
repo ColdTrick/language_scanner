5e2d12ecfba56eccf32dae3afe04f5c6b996f984
Rick ter Heide <Rick ter Heide@10.28.34.219>
2012-05-03 16:04:26 +0200

    Added dutch translation

---
+++ languages/nl.php
@@ -0,0 +1,12 @@
+<?php
+$language = array (
+  'language_scanner' => 'Vertalings scanner',
+  'admin:language' => 'Vertalingen',
+  'admin:language:scan' => 'Scan een plugin',
+  'admin:administer_utilities:language_scanner' => 'Vertalings scanner',
+  'language_scanner:admin:pick_plugin' => 'Kies een plugin om deze te scannen voor ongebruikte vertalings keys',
+  'language_scanner:result:title' => 'Scan resultaten voor: %s',
+  'language_scanner:result:total_keys' => 'Totaal aantal keys: %d',
+  'language_scanner:result:unused_keys' => 'Ongebruikte keys: %d',
+);
+add_translation("nl", $language);
