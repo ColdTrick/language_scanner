fd9ee8321707fb1662f056b5def0c6706fe60e93
Jerome Bakker <jeabakker@coldtrick.com>
2016-03-08 13:19:04 +0100

    fixed: load lightbox js/css

--- views/default/admin/administer_utilities/language_scanner.php
+++ views/default/admin/administer_utilities/language_scanner.php
@@ -1,19 +1,26 @@
 <?php
+
+elgg_load_js('lightbox');
+elgg_load_css('lightbox');
+
 $plugins = elgg_get_plugins('all');
 
 $ordered_plugins = [];
 foreach ($plugins as $plugin) {
 	$friendly_name = $plugin->getFriendlyName();
-	$ordered_plugins[$friendly_name] = elgg_view('output/url', [
+	$ordered_plugins[$friendly_name] = elgg_format_element('li', [], elgg_view('output/url', [
 		'text' => $friendly_name,
-		'href' => 'ajax/view/language_scanner/report?plugin_name=' . $plugin->getID(),
+		'href' => "ajax/view/language_scanner/report?plugin_name={$plugin->getID()}",
 		'class' => 'elgg-lightbox',
-		'data-colorbox-opts' => json_encode(['width' => 750, 'maxHeight' => '80%']),
-	]);
+		'data-colorbox-opts' => json_encode([
+			'width' => 750,
+			'maxHeight' => '80%',
+		]),
+	]));
 }
 
 uksort($ordered_plugins, 'strcasecmp');
 
-$body = elgg_format_element('ul', [], implode('</li><li>', $ordered_plugins));
+$body = elgg_format_element('ul', [], implode('', $ordered_plugins));
 
 echo elgg_view_module('inline', elgg_echo('language_scanner:admin:pick_plugin'), $body);
