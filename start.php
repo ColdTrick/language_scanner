53a590c5412d81566c82204f31f0ed6a0ea55896
Jeroen Dalsem <jdalsem@coldtrick.com>
2018-03-01 14:35:53 +0100

    chore: replaced deprecated menu item registration

--- start.php
+++ start.php
@@ -6,7 +6,14 @@
  * @return void
  */
 function language_scanner_init() {
-	elgg_register_admin_menu_item('administer', 'language_scanner', 'administer_utilities');
+	elgg_register_menu_item('page', [
+		'name' => 'administer_utilities:language_scanner',
+		'text' => elgg_echo('admin:administer_utilities:language_scanner'),
+		'href' => 'admin/administer_utilities/language_scanner',
+		'context' => 'admin',
+		'parent_name' => 'administer_utilities',
+		'section' => 'administer',
+	]);
 	
 	elgg_register_ajax_view('language_scanner/report');
 }
