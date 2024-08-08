120b8000737ab389c0d04da72f2b34dcf2aa53b5
Jerôme Bakker <jeabakker@coldtrick.com>
2024-08-08 15:04:40 +0200

    chore: updated Dutch translation

--- languages/nl.php
+++ languages/nl.php
@@ -1,10 +1,13 @@
 <?php
 /**
- * This file was created by Translation Editor v11.1
- * On 2023-05-17 14:46
+ * This file was created by Translation Editor v14.0
+ * On 2024-08-08 15:03
  */
 
 return array (
+  'language_scanner:report:core' => 'Elgg core',
+  'language_scanner:report:core:description' => 'Scan de Elgg core bestanden voor ongebruikte sleutels',
+  'language_scanner:admin:pick_plugin' => 'Kies een plugin om deze te scannen voor ongebruikte vertalingssleutels',
   'language_scanner:result:show_more' => '... toon meer',
   'language_scanner:result:code_keys' => 'Unieke vertaalsleutels gebruikt in plugin code: %d',
   'language_scanner:result:code_keys:untranslatable' => 'Unieke vertaalsleutels welke niet zijn vertaald: %d',
@@ -14,11 +17,9 @@ return array (
   'language_scanner:result:core_suggestions:title' => 'Core suggesties',
   'language_scanner:result:core_suggestions:suggestion' => 'Gebruik %s voor %s',
   'language_scanner:result:core_suggestions:no_suggestion' => 'Geen suggesties gevonden',
-  'language_scanner' => 'Vertalings scanner',
   'admin:language' => 'Vertalingen',
   'admin:language:scan' => 'Scan een plugin',
   'admin:administer_utilities:language_scanner' => 'Vertalings scanner',
-  'language_scanner:admin:pick_plugin' => 'Kies een plugin om deze te scannen voor ongebruikte vertalings keys',
   'language_scanner:result:title' => 'Scan resultaten voor: %s',
   'language_scanner:result:total_keys' => 'Totaal aantal keys: %d',
   'language_scanner:result:unused_keys' => 'Ongebruikte keys: %d',
